95ef3880c4696070814a077fb025983f8a9c8580
Puripat Kitkarncharoensin <6242075826@cbs.chula.ac.th>
2024-05-24 21:22:29 +0700

    Initial Commit

---
+++ SwiftUISourceControl/SwiftUISourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISourceControlApp.swift
+//  SwiftUISourceControl
+//
+//  Created by Puripat Kitkarncharoensin on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
